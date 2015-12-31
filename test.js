6240558ae29d2427105e1b13ff4a344ec43c0d7e
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:43:37 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test(t => {
-	t.false(fn('http://sindresorhus.com'));
-	t.false(fn('https://sindresorhus.com'));
-	t.false(fn('file://sindresorhus.com'));
-	t.true(fn('//sindresorhus.com'));
-	t.true(fn('/foo/bar'));
-	t.true(fn('foo/bar'));
-	t.true(fn('foo'));
-	t.end();
+	t.false(m('http://sindresorhus.com'));
+	t.false(m('https://sindresorhus.com'));
+	t.false(m('file://sindresorhus.com'));
+	t.true(m('//sindresorhus.com'));
+	t.true(m('/foo/bar'));
+	t.true(m('foo/bar'));
+	t.true(m('foo'));
 });
