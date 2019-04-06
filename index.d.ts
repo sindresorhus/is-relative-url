88fd996b84805ab8737732cd46e6026ad1c70b6b
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 10:15:10 +0200

    Meta fixes

--- index.d.ts
+++ index.d.ts
@@ -10,7 +10,7 @@ import isRelativeUrl = require('is-relative-url');
 isRelativeUrl('foo/bar');
 //=> true
 
-isRelativeUrl('http://sindresorhus.com/foo/bar');
+isRelativeUrl('https://sindresorhus.com/foo/bar');
 //=> false
 
 isRelativeUrl('//sindresorhus.com');
