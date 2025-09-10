2481165efc6b2b83ae143c617f6756e6f7adc305
Sindre Sorhus <sindresorhus@gmail.com>
2025-09-10 17:28:39 +0700

    Add explicit tests for data URIs to clarify they are absolute
     #2

--- test.js
+++ test.js
@@ -11,6 +11,18 @@ test('main', t => {
 	t.true(isRelativeUrl('foo'));
 });
 
+test('data URIs are absolute', t => {
+	// Test the exact data URI from GitHub issue #2
+	t.false(isRelativeUrl('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAHklEQVQoz2NgAIP/YMBAPBjVMNAa/pMISNcwEoMVAH0ls03D44ABAAAAAElFTkSuQmCC'));
+
+	// Test various data URI formats
+	t.false(isRelativeUrl('data:text/plain;charset=utf-8,Hello%20World'));
+	t.false(isRelativeUrl('data:text/html,<h1>Test</h1>'));
+	t.false(isRelativeUrl('data:,Hello%2C%20World!'));
+	t.false(isRelativeUrl('data:text/javascript,console.log("test")'));
+	t.false(isRelativeUrl('data:application/json,{"test":true}'));
+});
+
 test('with allowProtocolRelative option', t => {
 	// Default behavior (allowProtocolRelative: true)
 	t.true(isRelativeUrl('//sindresorhus.com'));
