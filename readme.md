662c0a53c61ed56d6b9a03d463c076ff73321352
Sindre Sorhus <sindresorhus@gmail.com>
2025-09-10 15:21:58 +0700

    Add `allowProtocolRelative` option
    Fixes #2

--- readme.md
+++ readme.md
@@ -21,7 +21,36 @@ isRelativeUrl('https://sindresorhus.com/foo/bar');
 
 isRelativeUrl('//sindresorhus.com');
 //=> true
+
+isRelativeUrl('//sindresorhus.com', {allowProtocolRelative: false});
+//=> false
 ```
+
+## API
+
+### isRelativeUrl(url, options?)
+
+#### url
+
+Type: `string`
+
+The URL to check.
+
+#### options
+
+Type: `object`
+
+##### allowProtocolRelative
+
+Type: `boolean`\
+Default: `true`
+
+Allow [protocol-relative URLs](https://en.wikipedia.org/wiki/URL#Protocol-relative_URLs) (e.g., `//example.com`) to be considered relative.
+
+Setting this to `false` will treat protocol-relative URLs as absolute, which can be useful for security purposes when you want to ensure a URL won't redirect to an external domain.
+
+> [!NOTE]
+> Protocol-relative URLs are [technically relative](https://datatracker.ietf.org/doc/html/rfc3986#section-4.2) according to [RFC 3986](https://datatracker.ietf.org/doc/html/rfc3986), as they require the current page's protocol to resolve into absolute URLs. However, they can still navigate to external domains, which may be a security concern in certain contexts (e.g., preventing open redirects).
 
 ## Related
 
--- test.js
+++ test.js
@@ -10,3 +10,26 @@ test('main', t => {
 	t.true(isRelativeUrl('foo/bar'));
 	t.true(isRelativeUrl('foo'));
 });
+
+test('with allowProtocolRelative option', t => {
+	// Default behavior (allowProtocolRelative: true)
+	t.true(isRelativeUrl('//sindresorhus.com'));
+	t.true(isRelativeUrl('//sindresorhus.com', {}));
+	t.true(isRelativeUrl('//sindresorhus.com', {allowProtocolRelative: true}));
+
+	// Strict mode (allowProtocolRelative: false)
+	t.false(isRelativeUrl('//sindresorhus.com', {allowProtocolRelative: false}));
+	t.false(isRelativeUrl('//example.com/path', {allowProtocolRelative: false}));
+	t.false(isRelativeUrl('//example.com/path?query=1', {allowProtocolRelative: false}));
+
+	// Should not affect other URL types
+	t.true(isRelativeUrl('/foo/bar', {allowProtocolRelative: false}));
+	t.true(isRelativeUrl('foo/bar', {allowProtocolRelative: false}));
+	t.true(isRelativeUrl('foo', {allowProtocolRelative: false}));
+	t.false(isRelativeUrl('http://example.com', {allowProtocolRelative: false}));
+	t.false(isRelativeUrl('https://example.com', {allowProtocolRelative: false}));
+
+	// Data URIs should still be absolute
+	t.false(isRelativeUrl('data:text/plain,Hello', {allowProtocolRelative: false}));
+	t.false(isRelativeUrl('data:text/plain,Hello', {allowProtocolRelative: true}));
+});
